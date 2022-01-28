7ccbad4a9735752a7acac080c0405f9bbf27a895
dedzen <77878255+dedzen@users.noreply.github.com>
2022-01-28 10:23:44 +0200

    Update webhook_utils.py

--- webhook_utils.py
+++ webhook_utils.py
@@ -16,7 +16,10 @@ async def create_webhook_if_not_exist(client, id: int) -> str:
     except Exception as e:
         log_error(channel.guild, e, 1)
 def send_with_webhook(url, content, server, name,  avatar_url, attachaments):
-    webhook = DiscordWebhook(url=url, content=content, username=f"{name} | {server}", avatar_url=f"{avatar_url}")
+     allowed_mentions = {
+        "parse": []
+    }
+    webhook = DiscordWebhook(url=url, content=content, username=f"{name} | {server}", avatar_url=f"{avatar_url}", allowed_mentions=allowed_mentions)
     if attachaments:
         embed = DiscordEmbed()
         embed.set_image(url=attachaments[0].url)
